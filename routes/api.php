edb25ff8a1c478ca9b8ae9f83304815ba7aac8c7
Ebube Life <ebubeemeka19@gmail.com>
2023-06-26 20:53:09 +0100

    ...

--- routes/api.php
+++ routes/api.php
@@ -1,3 +1,6 @@
+<?php  
+
+
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\MembersController;
@@ -40,3 +43,4 @@ Route::middleware('auth:sanctum')->group(function () {
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
 });
+?>
