1ff97e9f9907d223bf41259e07163122e1c95907
Ebube Life <ebubeemeka19@gmail.com>
2023-06-26 20:50:53 +0100

    ...

--- routes/api.php
+++ routes/api.php
@@ -1,5 +1,3 @@
-<?php
-
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\MembersController;
@@ -21,36 +19,24 @@ Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
 });
 
-Route::controller(MembersController::class)->group(function(){
-    Route::post('signup', 'store');
-    Route::post('login', 'login');
-    Route::post('verify_email_otp', 'verify_email_otp');
-    Route::post('send_email', 'send_email');
-    Route::get('test','test');
-   // Route::middleware('auth:sanctum')-> post('set_transaction_pin','set_transaction_pin');
-   
-   // Route::get('test_api','test_api');
+Route::prefix('members')->group(function () {
+    Route::post('signup', [MembersController::class, 'store']);
+    Route::post('verify_email_otp', [MembersController::class, 'verify_email_otp']);
+    Route::post('send_email', [MembersController::class, 'send_email']);
+    Route::get('test', [MembersController::class, 'test']);
 });
 
-Route::get('/grabber', [DownloadController::class,'index']);
+Route::get('/grabber', [DownloadController::class, 'index']);
 
-Route::get('/convert', [DownloadController::class,'index']);
+Route::get('/convert', [DownloadController::class, 'index']);
 
-Route::get('/test_api', [DownloadController::class,'test_rapid_api']);
+Route::get('/test_api', [DownloadController::class, 'test_rapid_api']);
 
-
-
-
-
-
- Route::middleware('auth:sanctum')->group(function () {
-        Route::post('generate_summary', [TaskController::class, 'generateVideoSummary']);
-        // other protected routes...
-    });
+Route::middleware('auth:sanctum')->group(function () {
+    Route::post('generate_summary', [TaskController::class, 'generateVideoSummary']);
+    // other protected routes...
+});
 
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
-        return $request->user();
-    });
-
-
-
+    return $request->user();
+});
