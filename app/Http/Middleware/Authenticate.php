94f2e5ea00ac04a23ac7a1d8817635744d93444f
Ebube Life <ebubeemeka19@gmail.com>
2023-06-26 20:58:03 +0100

    ...

--- app/Http/Middleware/Authenticate.php
+++ app/Http/Middleware/Authenticate.php
@@ -2,9 +2,20 @@
 
 namespace App\Http\Middleware;
 
-protected function redirectTo($request)
+use Illuminate\Auth\Middleware\Authenticate as Middleware;
+
+class Authenticate extends Middleware
 {
-    if (! $request->expectsJson()) {
-        return response()->json(['error' => 'Unauthenticated.'], 401);
+    /**
+     * Get the path the user should be redirected to when they are not authenticated.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return string|null
+     */
+    protected function redirectTo($request)
+    {
+        if (! $request->expectsJson()) {
+            return response()->json(['error' => 'Unauthenticated.'], 401);
+        }
     }
 }
