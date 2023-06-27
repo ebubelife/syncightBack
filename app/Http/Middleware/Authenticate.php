18f6383352d16e8be82680ef5c20446aa8fddf92
Ebube Life <ebubeemeka19@gmail.com>
2023-06-27 01:03:26 +0100

    ...

--- app/Http/Middleware/Authenticate.php
+++ app/Http/Middleware/Authenticate.php
@@ -1,27 +1,21 @@
 <?php
+
 namespace App\Http\Middleware;
 
-use Closure;
 use Illuminate\Auth\Middleware\Authenticate as Middleware;
 
 class Authenticate extends Middleware
 {
     /**
-     * Handle an unauthenticated request.
+     * Get the path the user should be redirected to when they are not authenticated.
      *
      * @param  \Illuminate\Http\Request  $request
-     * @param  \Closure  $next
-     * @param  array  $guards
-     * @return mixed
+     * @return string|null
      */
-    public function handle($request, Closure $next, ...$guards)
+    protected function redirectTo($request)
     {
-        $this->authenticate($request, $guards);
-
-        if ($request->expectsJson() && ! $request->user()) {
-            return response()->json(['error' => 'Unauthenticated'], 401);
+        if (! $request->expectsJson()) {
+            return null; // Remove the reference to 'login'
         }
-
-        return $next($request);
     }
 }
