005ee4e1df6f2a8bb2b144893505570021200035
Ebube Life <ebubeemeka19@gmail.com>
2023-07-09 17:04:52 +0100

    ...

--- app/Http/Controllers/TaskController.php
+++ app/Http/Controllers/TaskController.php
@@ -235,7 +235,7 @@ class TaskController extends Controller
              
 
           
-            return response()->json(['textSummary'=>$response->summary]);
+            return response()->json(['textSummary'=>$response["summary"]]);
 
         }
 
