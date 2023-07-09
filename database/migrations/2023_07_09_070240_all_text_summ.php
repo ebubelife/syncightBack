e1d7f5b1a7aa37be888853b0ec1cfed087ddad1e
Ebube Life <ebubeemeka19@gmail.com>
2023-07-09 08:03:03 +0100

    ...

---
+++ database/migrations/2023_07_09_070240_all_text_summ.php
@@ -0,0 +1,40 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class AllTextSumm extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        //
+        Schema::create('text_summaries', function (Blueprint $table) {
+            $table->id();
+            $table->string('user_id');
+            $table->longText('text');
+            
+            $table->longText('summary');
+           
+            $table->boolean('email_verified');
+           
+           
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        //
+    }
+}
