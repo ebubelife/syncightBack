6318d486e4d937dcad3cbe7610d0fbde62d07364
Ebube Life <ebubeemeka19@gmail.com>
2023-07-14 12:58:11 +0100

    ....

---
+++ database/migrations/2023_07_14_114345_add_credit_count.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class AddCreditCount extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        //
+
+        Schema::table('members', function (Blueprint $table) {
+           
+            $table->string('credit_count')->default('10');
+          
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        //
+    }
+}
